1e0f8e1a83e211841795c43d91a87b17a56dcd2a
Вадим Желтов <99135277+ZhVadim@users.noreply.github.com>
2022-04-07 23:54:00 +0300

    Желтов ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,35 +7,20 @@ int cbinsearch(int *arr, int size, int value) {
     for (int i = 0; i < K; i++) {
         int C = N + (K - N) / 2;
         if (arr[C] == value) {
-            if (arr[C - 1] != value && arr[C + 1] != value) {
-                kol += 1;
-                return kol;
-            } 
-            else if (arr[C - 1] = value && arr[C + 1] != value) {
+            if (arr[C - 1] != value || arr[C + 1] != value) {
                 for (int j = C; arr[j] == value; j--) {
                     kol += 1;
                 }
-                return kol;
-            } 
-            else if (arr[C - 1] != value && arr[C + 1] == value) {
-                for (int p = C; arr[p] == value; p++) {
-                    kol += 1;
-                }
-                return kol;
-            } else {
-                for (int j = C; arr[j] == value; j--) {
-                    kol += 1;
-                }
-                for (int p = C; arr[p] == value; p++) {
+                for (int p = C + 1; arr[p] == value; p++) {
                     kol += 1;
                 }
                 return kol;
             }
-        } 
-        else if (arr[C] < value) {
+        } else if (arr[C] < value) {
             N = C + 1;
         } else {
             K = C;
         }
     }
+    return 0;
 }
